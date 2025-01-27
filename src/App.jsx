09b5b003b5cf82773e2a8459029981a9358ef26c
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-27 23:28:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <p> Hello inwood family</p>
+    </div>
+  );
+}
+export default App;
