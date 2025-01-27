90146e916ebda5ffe1d4803e3df0283fc6d06530
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-27 23:56:05 +0100

    line test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="bg-blue-500 text-white p-4">
       <p className="flex justify-center items-center text-amber-700"> Hello inwood family</p>
+      <span>
+        <a href="https://inwood.dev" className="underline">
+          Inwood.dev
+        </a>
+      </span>
     </div>
   );
 }
