8ddd7d95c3010890b7c0a78a0ddd0139535cb54c
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-11 23:56:18 +0100

    working on nav

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <a href="">
+  return (
+    <header>
+      <div className="flex justify-between items-center mt-5">
+        <nav className="flex items-center gap-10">
+          <a href="#">
+            <img src="./images/herologo.png" alt="logo" />
+          </a>
+          <ul className="flex gap-10">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Shop</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* <a href="">
                 <img src="./images/bghero.png" alt="" />
-            </a>
-        </header>
-    );
+            </a> */}
+    </header>
+  );
 };
 
 export default Header;
