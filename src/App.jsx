b5d4a82822b4f987b4a481f76854f93b2e5eb626
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-11 23:24:19 +0100

    adding background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import React from "react";
 function App() {
   return (
     <div className="bg-blue-500 text-white p-4">
-      <p className="flex justify-center items-center text-amber-700"> Hello inwood family</p>
+      <p className="flex justify-center items-center text-amber-800"> Hello inwood family</p>
       <span>
         <a href="https://inwood.dev" className="underline">
           Inwood.dev
         </a>
+        <img src="./assets/images/bghero.png" alt="" />
       </span>
     </div>
   );
