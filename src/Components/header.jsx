5936fa63d609510a35a1be0577400813a16f8aa7
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-12 11:03:05 +0100

    nav bar

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,31 +2,26 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <div className="flex justify-between items-center mt-5">
-        <nav className="flex items-center gap-10">
-          <a href="#">
-            <img src="./images/herologo.png" alt="logo" />
-          </a>
-          <ul className="flex gap-10">
-            <li>
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">Shop</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Contact</a>
-            </li>
+    <header className="fixed top-0 left-0 z-50 w-full transition-all duration-500">
+      <div className="container flex mt-7">
+        <a href="#" className="cursor-pointer z-2">
+          <img src="./images/herologo.png" alt="logo" />
+        </a>
+        <nav className="flex items-center w-full">
+          <ul className="flex font-semibold items-center gap-10 md:m-auto lg:m-auto xl:m-auto">
+            <li>Home</li>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contact</li>
           </ul>
+          <span className="flex items-center gap-10">
+            <img src="./icons/profile.svg" alt="profle" />
+            <img src="./icons/search.svg" alt="search" />
+            <img src="./icons/liked.svg" alt="love" />
+            <img src="./icons/cart.svg" alt="cart" />
+          </span>
         </nav>
       </div>
-      {/* <a href="">
-                <img src="./images/bghero.png" alt="" />
-            </a> */}
     </header>
   );
 };
