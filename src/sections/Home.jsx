e987eff6327a281468dc057a8f709aa82dc6657f
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-12 14:35:58 +0100

    home background added

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-      <main>
-        <div>
-            <h1>Home</h1>
+  return (
+    <main className="relative w-full h-screen">
+      {/* Background Image */}
+      <img 
+        src="./images/bg.png" 
+        alt="Furniture" 
+        className="w-full h-full object-cover"
+      />
+
+      {/* Overlay Text */}
+      <div className="absolute inset-0 flex flex-col justify-self-end my-78  md:m-30  text-white bg-opacity-50">
+        <div className="bg-[#FFF3E3] max-w-lg w-full p-2 md:p-12 mt-20">
+        <p className="text-[#B88e2f] text-xs pt-2.5 font-bold uppercase mb-5">New Arrival</p>
+        <h1 className="text-[#BC943A] text-3xl md:text-4xl font-extrabold">Discover Our New Collection</h1>
+        <p className="text-black mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempore riam aliquam itaque nemo ullam sit. Fugit.</p>
         </div>
-      </main>
-    );
+      </div>
+    </main>
+  );
 };
 
 export default Home;
