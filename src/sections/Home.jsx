5a66e478e32b01729cf7b8aed2a9fbfb658b5c98
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-13 22:37:37 +0100

    adding btn

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -15,7 +15,8 @@ const Home = () => {
         <div className="bg-[#FFF3E3] max-w-lg w-full p-2 md:p-12 mt-20">
         <p className="text-[#B88e2f] text-xs pt-2.5 font-bold uppercase mb-5">New Arrival</p>
         <h1 className="text-[#BC943A] text-3xl md:text-4xl font-extrabold">Discover Our New Collection</h1>
-        <p className="text-black mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempore riam aliquam itaque nemo ullam sit. Fugit.</p>
+        <p className="text-black mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempore riam aliquam itaque nemo ullam sit. Fugit.</p>
+        <button className="bg-[#BC943A] text-white px-4 py-2 mt-5">Buy Now</button>
         </div>
       </div>
     </main>
